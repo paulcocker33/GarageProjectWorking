d92ea7ed6a2bb5ebc84d2a8b6b592e50dfe9e7fd
Paul Cocker <paulrobert.cocker@gmail.com>
2018-05-28 08:40:59 +1200

    Update

---
+++ GarageControllerApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class DoorController(models.Model):
+    name = models.TextField()
+    street = models.TextField(blank=True, null=True)
+    number = models.TextField(blank=True, null=True)
+    city = models.TextField(blank=True, null=True)
+    uniqueID = models.TextField
