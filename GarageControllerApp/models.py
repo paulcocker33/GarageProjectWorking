48f383079484ad1d47e361fb65371f8e0ae9ca08
Paul Cocker <paulrobert.cocker@gmail.com>
2018-05-28 10:24:37 +1200

    Removed Junk Migrations

--- GarageControllerApp/models.py
+++ GarageControllerApp/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 # Create your models here.
-class DoorController(models.Model):
+class Controller_Type(models.Model):
+    name = models.CharField(max_length=30)
+
+class Door_Controller(models.Model):
     name = models.TextField(blank=False)
     street = models.TextField(blank=True, null=True)
     number = models.TextField(blank=True, null=True)
@@ -10,10 +13,12 @@ class DoorController(models.Model):
     uniqueID = models.CharField(max_length=20,blank=False, null=False)
     device_Online = models.BooleanField
     create_date = models.DateTimeField('date added')
+    controller_type = models.ForeignKey(Controller_Type, on_delete=models.CASCADE)
 
-class GarageUser(models.Model):
+
+class Garage_User(models.Model):
     name = models.TextField()
     email_address = models.TextField(blank=False, null=False)
     password = models.CharField(max_length=20,blank=False, null=False)
-    registered_devices= [models.ForeignKey(DoorController, on_delete=models.CASCADE)]
+    registered_devices= [models.ForeignKey(Door_Controller, on_delete=models.CASCADE)]
     create_date = models.DateTimeField('date added')
