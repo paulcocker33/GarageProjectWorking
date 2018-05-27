1fa507a668a4a2fb2578a0beeeabaa36ee1e1656
Paul Cocker <paulrobert.cocker@gmail.com>
2018-05-28 09:52:53 +1200

    update

--- GarageControllerApp/models.py
+++ GarageControllerApp/models.py
@@ -3,10 +3,17 @@ from django.db import models
 
 # Create your models here.
 class DoorController(models.Model):
-    name = models.TextField()
+    name = models.TextField(blank=False)
     street = models.TextField(blank=True, null=True)
     number = models.TextField(blank=True, null=True)
     city = models.TextField(blank=True, null=True)
-    uniqueID = models.TextField
+    uniqueID = models.CharField(max_length=20,blank=False, null=False)
+    device_Online = models.BooleanField
+    create_date = models.DateTimeField('date added')
 
-
+class GarageUser(models.Model):
+    name = models.TextField()
+    email_address = models.TextField(blank=False, null=False)
+    password = models.CharField(max_length=20,blank=False, null=False)
+    registered_devices= [models.ForeignKey(DoorController, on_delete=models.CASCADE)]
+    create_date = models.DateTimeField('date added')
