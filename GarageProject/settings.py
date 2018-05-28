ba7981dab2eedaff24a42704738a327c2994b253
Paul Cocker <paulrobert.cocker@gmail.com>
2018-05-28 15:11:24 +1200

    Fixed DB

--- GarageProject/settings.py
+++ GarageProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'GarageControllerApp.apps.GaragecontrollerappConfig',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
